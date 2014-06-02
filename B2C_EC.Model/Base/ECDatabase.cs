15c55db8295e337a5846043b6d1ab6e8fc91355c
htphongqn <htphongqn@gmail.com>
2014-06-02 13:36:53 +0700

    Init

---
+++ B2C_EC.Model/Base/ECDatabase.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+namespace B2C_EC.Model.Base
+{
+    public class ECDatabase : DbContext
+    {
+
+    }
+}
---
+++ B2C_EC.Model/Migrations/Configuration.cs
@@ -0,0 +1,31 @@
+namespace B2C_EC.Model.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<B2C_EC.Model.Base.ECDatabase>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(B2C_EC.Model.Base.ECDatabase context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data. E.g.
+            //
+            //    context.People.AddOrUpdate(
+            //      p => p.FullName,
+            //      new Person { FullName = "Andrew Peters" },
+            //      new Person { FullName = "Brice Lambson" },
+            //      new Person { FullName = "Rowan Miller" }
+            //    );
+            //
+        }
+    }
+}
