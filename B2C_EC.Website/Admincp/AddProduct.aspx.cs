f5d1a70bbe9930d518146d4379ef4c793fec8aeb
Bùi Văn Chí <bvchi2907@gmail.com>
2014-06-13 15:21:33 +0700

    Add product not file image

--- B2C_EC.Website/Admincp/AddProduct.aspx.cs
+++ B2C_EC.Website/Admincp/AddProduct.aspx.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using B2C_EC.Model;
+using B2C_EC.Model.Data;
+using B2C_EC.Model.Global;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,9 +12,44 @@ namespace B2C_EC.Website.Admincp
 {
     public partial class AddProduct : System.Web.UI.Page
     {
+        private ProductRepo productRepo = new ProductRepo();
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (!IsPostBack)
+            {
+                LoadManufacturerAndProductType();
+            }
+        }
 
+        private void LoadManufacturerAndProductType()
+        {
+            ddlManufacturer.DataSource = (new ManufacturerRepo()).GetAllManufacturers();
+            ddlManufacturer.DataBind();
+            ddlProductType.DataSource = (new ProductTypeRepo()).GetAllProductType();
+            ddlProductType.DataBind();
+        }
+
+        protected void btnSave_Click(object sender, EventArgs e)
+        {
+            Product p = new Product();
+            p.Name = txtName.Text;
+            p.PriceOld = ToSQL.SQLToDecimal(txtPriceOld.Text);
+            p.PriceNew = ToSQL.SQLToDecimal(txtPriceNew.Text);
+            p.ProductType_ID = ToSQL.SQLToInt(ddlProductType.SelectedValue);
+            p.Manufacuturer_ID = ToSQL.SQLToInt(ddlManufacturer.SelectedValue);
+            p.Description = CKEditorControlDescription.Text;
+            p.IsActive = chkActive.Checked;
+            p.IsBestSelling = chkBestSelling.Checked;
+            p.IsNew = chkNew.Checked;
+            p.IsSpecial = chkSpecial.Checked;
+            if (productRepo.CreateProduct(p) > 0)
+            {
+                Response.Redirect("~/Admincp/Management-Products.aspx");
+            }
+            else
+            {
+                lblError.Text = "Please check input data! Try again!";
+            }
         }
     }
 }
