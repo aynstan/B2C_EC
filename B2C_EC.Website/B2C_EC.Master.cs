72bf9696d4a48feed1ccee9746f9d99e91099054
htphongqn <htphongqn@gmail.com>
2014-06-14 11:47:42 +0700

    add page customerInfo

--- B2C_EC.Website/B2C_EC.Master.cs
+++ B2C_EC.Website/B2C_EC.Master.cs
@@ -56,7 +56,7 @@ namespace B2C_EC.Website
                 lnkSignOut.Visible = true;
                 lnkSignIn.Visible = false;
                 lblMember.Visible = true;
-                lblMember.Text = "Welcome, " + ((Customer)Session["Customer"]).FirstName + "!";
+                lblMember.Text = "Welcome,<a href='CustomerInfo.aspx'>" + ((Customer)Session["Customer"]).FirstName + "!</a>";
             }
         }
 
